5574458749d6792cff7a771e400c4a97221a7bf2
wsxingjun <1185889308@qq.com>
2017-11-17 09:26:47 +0800

    v1-查询到了contact—_id，知道联系人中存在几个联系人

---
+++ src/main/java/it/oztaking/com/a51_contentprovidercontactsoperation/MainActivity.java
@@ -0,0 +1,61 @@
+package it.oztaking.com.a51_contentprovidercontactsoperation;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.id;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt_QueryContacts = (Button) findViewById(R.id.bt_QueryContacts);
+        Button bt_InsertContacts = (Button) findViewById(R.id.bt_InsertContacts);
+        bt_InsertContacts.setOnClickListener(this);
+        bt_QueryContacts.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.bt_QueryContacts:
+                QueryContacts();
+                break;
+            case R.id.bt_InsertContacts:
+                InsertContacts();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void QueryContacts() {
+
+        //1.根据raw_contacts表中的contact_id得知其中存在几条联系人
+        //使用内容解析者取数据
+        Uri uri = Uri.parse("content://com.android.contacts/raw_contacts");
+        Cursor cursor = getContentResolver().query(uri, new String[]{"contact_id"}, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String contact_id = cursor.getString(0);
+                System.out.println("contact_id:" + contact_id);
+            }
+        }
+        //2.根据contact_id在data表中查询minitype_id和data1的内容；
+
+    }
+
+    public void InsertContacts() {
+
+    }
+}
